a979c8cce999c77c73b008d2155b13d572dda2be
GeorgeContreras241 <maicolcontreras241@gmail.com>
2025-03-10 02:04:48 -0500

    Nuevos Projectos, mejoras de css en version movil

--- src/components/CardProject.jsx
+++ src/components/CardProject.jsx
@@ -1,41 +1,50 @@
 import { useState } from "react"
 
-export const CardProject = () => {
+
+export const CardProject = ({ data }) => {
    const [open, setOpen] = useState(false);
    return (
-      <article className={open ? "card-project open" : "card-project"}>
+      <article className={open ? "card-project open" : "card-project"} key={data.id}>
          <div className="card-project-inner">
             <div className="flip-card-front">
                <div className="card-image">
                   <img
-                     src="https://porfolio-george-contreras.vercel.app/images/project05.jpg"
-                     alt=""
+                     src={data.image}
+                     alt={data.title}
                   />
                </div>
-               <p className="card-front-title">Api Real Faker Eccomers</p>
-               <span>culminado 24/08/32</span>
+               <section>
+                  <p className="card-front-title">{data.title}</p>
+                  <span>Culminado : {data.date}</span>
+               </section>
                <p className="card-front-desp">
-                  En ete proyecto se consume el Api Fake Store de manera realista, se realiza con 
-                  redux y react , demas tecnologias y pocibles causa de res.
+                  {data.description}
                </p>
                <div className="card-front-buttoms">
-                  <button onClick={() => setOpen(!open)}>Ver mas</button>
+                  <button onClick={() => setOpen(!open)} class="button-ver-mas">
+                     <span class="label">Ver Mas</span>
+                     <span class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"></path><path fill="currentColor" d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"></path></svg>
+                     </span>
+                  </button>
+
                </div>
             </div>
             <div className="flip-card-back">
-               <p>nombre del proyecto</p>
-               <span>tecnologias utilizadas</span>
-               <ul>
-                  <li>React</li>
-                  <li>Next.js</li>
-                  <li>Tailwind CSS</li>
-                  <li>Node.js</li>
-                  <li>MongoDB</li>
-                  <li>Express.js</li>
-               </ul>
-               <button>github</button>
-               <button>visitae</button>
-               <button onClick={()=>setOpen(!open)}>atras</button>
+               <section>
+                  <p>{data.title}</p>
+                  <span>Tecnologias</span>
+                  <ul>
+                     {data.tec.map((t) => (
+                        <li key={t}>{t}</li>
+                     ))}
+                  </ul>
+               </section>
+               <div className="flip-card-back-buttons">
+                  <a href={data.git} target="_blank">Git Hub</a>
+                  <a href={data.url} target="_blank">Visitar</a>
+                  <a onClick={() => setOpen(!open)}>Atras</a>
+               </div>
             </div>
          </div>
       </article>
